308865b7c891854be6373a3c48af87f63f6e3e0e
Daniel Mendoza <danielmendoza@Daniels-MacBook-Air.local>
2024-02-20 10:06:37 -0500

    Add field box widget

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MessageFieldBox());
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello from box");
+  }
+}
