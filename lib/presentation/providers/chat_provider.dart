af2e154bb3bb86cfd00261850f16c75b2a29ce2f
Daniel Mendoza <danielmendoza@Daniels-MacBook-Air.local>
2024-02-20 20:25:52 -0500

    Add provider in message bubbles

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: "Hola", fromWho: FromWho.me),
-    Message(text: "Cómo estás?", fromWho: FromWho.me)
+    Message(text: "Cómo estás?", fromWho: FromWho.me),
+    Message(text: "Qué onda", fromWho: FromWho.other)
   ];
 
   Future<void> sendMessage(String text) async {
